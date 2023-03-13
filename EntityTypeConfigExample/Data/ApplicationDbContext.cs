0f27ee5b563b3b2773ccdec63100f5b5b948314d
Michael Gardner <mikeyo241@live.com>
2023-03-13 09:32:35 -0400

    Add project files.

---
+++ EntityTypeConfigExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EntityTypeConfigExample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityTypeConfigExample.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        // Add the DbSet for the Person model.
+        // Tell Entity Framework that the Person model is a table in the database.
+        public DbSet<Person> People { get; set; }
+        // Override the OnModelCreating method to apply the EntityTypeConfiguration.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Call the base method to apply the Identity Tables from the IdentityDbContext.
+            base.OnModelCreating(modelBuilder);  // Apply the Identity Tables from the IdentityDbContext.
+            // Continue with the applications tables.
+            // This Applys each table in a way that allows the ForeignKeys to be set.
+            modelBuilder.ApplyConfiguration(new Person());
+        }
+        // Add the constructor to pass the DbContextOptions to the base class.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ EntityTypeConfigExample/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityTypeConfigExample.Models
+{
+    public class Person : IEntityTypeConfiguration<Person>
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        // https://learn.microsoft.com/en-us/dotnet/api/system.data.entity.modelconfiguration.entitytypeconfiguration-1?view=entity-framework-6.2.0
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            // Customize the models table name
+            builder.ToTable("PERSON_TABLE");
+            // Add a table comment
+            builder.ToTable(p => p.HasComment("Information about a person."));
+            // Setup the primary key
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+            // Setup the required fields
+            builder.Property(p => p.FirstName).IsRequired(true);
+            builder.Property(p=> p.LastName).IsRequired(true);
+            // Set default values for nullable fields
+            builder.Property(p => p.Title).HasDefaultValue("");
+            builder.Property(p => p.Email).HasDefaultValue("");
+        }
+    }
+}
